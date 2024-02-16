cf07d975f4d66a2c1075e15696d98f86a79f1d2d
sejapoe <bekaldiev2015@gmail.com>
2024-02-16 16:17:50 +0300

    chore(git): initial commit

---
+++ src/main/java/ru/sejapoe/dz1/Dz1Application.java
@@ -0,0 +1,13 @@
+package ru.sejapoe.dz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sejapoe/dz1/Dz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sejapoe.dz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
