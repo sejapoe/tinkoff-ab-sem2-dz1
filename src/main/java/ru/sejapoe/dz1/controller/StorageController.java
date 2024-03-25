52dd0642973d0a8c17f0fb598eeba5c48352888e
sejapoe <bekaldiev2015@gmail.com>
2024-03-25 20:57:28 +0300

    chore: change file extension check to use apache.commons.io

--- src/main/java/ru/sejapoe/dz1/controller/StorageController.java
+++ src/main/java/ru/sejapoe/dz1/controller/StorageController.java
@@ -1,6 +1,7 @@
 package ru.sejapoe.dz1.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,29 +11,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import ru.sejapoe.dz1.storage.StorageService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class StorageController {
-    private static final List<String> imageExtensions = List.of("png", "jpg", "gif");
+    private static final List<String> imageExtensions = List.of("png", "jpg", "gif", "jpeg");
     private final StorageService storageService;
 
     @GetMapping(value = "/images/{filename:.+}")
     public ResponseEntity<Resource> serveImage(@PathVariable String filename) {
         Resource file = storageService.loadAsResource(filename);
 
-        if (file == null)
+        if (file == null) {
             return ResponseEntity.notFound().build();
+        }
 
-        var ext = Optional.ofNullable(filename)
-                .filter(f -> f.contains("."))
-                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+        var ext = FilenameUtils.getExtension(filename);
 
-        if (ext.isEmpty() || imageExtensions.stream().noneMatch(ext.get()::equals))
+        if (ext.isBlank() || imageExtensions.stream().noneMatch(ext::equals)) {
             return ResponseEntity.badRequest().build();
+        }
 
-        MediaType mediaType = MediaType.parseMediaType("image/" + ext.get());
+
+        MediaType mediaType = MediaType.parseMediaType("image/" + ext);
 
         return ResponseEntity.ok()
                 .contentType(mediaType)
